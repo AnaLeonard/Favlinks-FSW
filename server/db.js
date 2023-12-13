199c053d0bc70cf6850c2603f658486b821ea622
Karen Leonardo <ana.leonardo@lc.cuny.edu>
2023-12-12 22:52:18 -0500

    Adding the get, and other post methods

--- server/db.js
+++ server/db.js
@@ -1,35 +1,3 @@
-// const Pool = require('pg').Pool
-//     const pool = new Pool({
-//     user: 'analeonardo',
-//     host: 'localhost',
-//     database: 'linksapi',
-//     password: '2002',
-//     port: 5432,
-//     })
-    
-//     // GET /api/links Get all the links from the database
-//     app.get('/api/links', (req, res) => {
-//         pool.query('SELECT links FROM linksapi', (error, results) => {
-//         if (error) {
-//         throw error
-//         }
-//         res.status(200).json(results.rows)
-//         })
-//         })
-    
-//         //insert new link using post
-//     app.post('/api/links', (req, res) => {
-//         const {name, url} = req.body;
-//         const query = 'INSERT INTO favlinks (name, url) VALUES ($1, $2)  RETURNING *';
-//         pool.query(query, [name, url], (error, result) => {
-//         if (error) {
-//           throw error;
-//         }
-//         res.status(201).json(result.rows[0]);
-//     });
-//     });
-
-
 const Pool = require('pg').Pool
 const pool = new Pool({
 user: 'analeonardo',
@@ -39,14 +7,66 @@ password: '2002',
 port: 5432,
 })
 
+// GET all links
 const getLinks = (req, res) => {
-    pool.query('SELECT * FROM favlinks ORDER BY id ASC', (error, result) => {
-    if (error) {
-    throw error
+    pool.query('SELECT * FROM favlinks', (error, results) => {
+        if (error) {
+          throw error;
+        }
+        const links = results.rows;
+        res.status(200).json(links);
+      });
     }
-    res.status(200).json(result.rows)
-    })
-    }
+  
+  // POST to create a new link
+
+  const addLink = (req, res) => {
+    const { name, url } = req.body;
+    pool.query('INSERT INTO favlinks (name, url) VALUES ($1, $2) RETURNING *',
+     [name, url], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      const newLink = results.rows[0];
+      res.status(201).json(newLink);
+    });
+  }
+
+  
+  // POST to update a link with a specific id
+  const updateLink = (req, res) => {
+    const { name, url } = req.body;
+    //gets the id
+    const id = req.params.id;
+    pool.query('UPDATE favlinks SET name = $1, url = $2 WHERE id = $3 RETURNING *', [name, url, id], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      const updatedLink = results.rows[0];
+      res.status(200).json(updatedLink);
+    });
+  }
+
+  
+  // POST to delete a link with a specific id
+  const deleteLink = (req, res)=> {
+    const id = parseInt(req.params.id);
+  
+    pool.query('DELETE FROM favlinks WHERE id = $1 RETURNING *', [id], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      const deletedLink = results.rows[0];
+      res.status(200).json(deletedLink);
+    });
+  }
+
+
     module.exports= {
     getLinks,
+    addLink,
+    updateLink,
+    deleteLink
+
     }
+
